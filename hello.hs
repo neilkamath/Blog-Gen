eafe53628bb8f7cb4cfd7c0ccf530494ad8fd4ec
unknown <neilkamath07@gmail.com>
2025-02-26 03:51:35 -0800

    initial

---
+++ hello.hs
@@ -0,0 +1,10 @@
+main = putStrLn myHtml
+
+html_ content = "<html>" <> content <> "</html>"
+body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title content = html_(head_ (title_ title) <> body_ content)
+
+myHtml = makeHtml "Hello title" "Hello World!"
