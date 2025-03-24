8ba2d6246430e7cf1346d9673cdf913bdabaadec
unknown <neilkamath07@gmail.com>
2025-03-24 04:32:49 -0700

    new types and html structures

--- hello.hs
+++ hello.hs
@@ -1,34 +1,51 @@
 main :: IO ()
-main = putStrLn myHtml
+main = putStrLn (render myhtml)
 
-myHtml :: String
-myHtml = 
-    makeHtml 
-        "Hello title" 
-        (h1_ "Hello, world!" <> p_ "Let's Learn Haskell")
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_(head_ (title_ title) <> body_ content)
+newtype Html = Html String
+newtype Structure = Structure String
+type Title = String
 
--- helpers
-html_ :: String -> String
-html_  = el "html"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      ( el "head" (el "title" title)
+        <> el "body" (getStructureString content)
+      )
+    )
 
-body_ :: String -> String
-body_ = el "body"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+    case html of
+        Html str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ a b = 
+    Structure (getStructureString a <> getStructureString b)
